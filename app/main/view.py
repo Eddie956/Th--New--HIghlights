778ba969054295e78c755b2bcc20796b9a2fc696
Eddie956 <mutugieddie3@gmail.com>
2018-09-02 09:25:10 +0300

    created a root page function index it will hreturn the page and its data in index.html
    created a root function for articles
    it will return the page and its data in articles.html

---
+++ app/main/view.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from . import main
+from ..request import get_sources, get_articles
+from ..models import Sources
+
+@main.route('/')
+def index():
+    title = "Welcome to the News page."
+
+    general = get_sources('general')
+    technology = get_sources('technology')
+    business = get_sources('business')
+    health = get_sources('health')
+    entertainment = get_sources('entertainment')
+    sports =get_sources('sports')
+
+    return render_template('index.html',title = title, general = general, technology = technology, business = business, health = health, entertainment = entertainment, sports = sports
+        )
+
+@main.route('/articles/<source_id>')
+def articles(source_id):
+    '''
+    Function that returns articles based on their sources
+    '''
+    news_source = get_articles(source_id)
+    title = 'Articles | News Articles'
+    return render_template('articles.html', title = title, name = source_id, news = news_source)
